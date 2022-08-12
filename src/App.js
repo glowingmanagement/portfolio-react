61ac5b43cb14fd471d979113a64707384e14de44
Josh Holmes <josh@glowingmanagement.com>
2022-08-12 07:06:17 +0100

    WIP

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import NavBar from "./containers/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Home</h1>
+    <div>
+      <NavBar />
     </div>
   );
 }
