a322f8100ce2a275dd5fb83d980c1db20b33c7fd
Josh Holmes <josh@glowingmanagement.com>
2022-08-18 07:48:36 +0100

    add footer links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,32 @@
 import "./Footer.css";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
 
 const Footer = () => {
   return (
     <div className="footerContainer">
-      <h1>Footer</h1>
+      <a
+        target="_blank"
+        href="https://www.github.com/glowingmanagement"
+        rel="noreferrer"
+      >
+        <GitHubIcon sx={{ fontSize: 35, margin: 3 }} />
+      </a>
+      <a
+        target="_blank"
+        href="https://www.linkedin.com/in/joshholmes22"
+        rel="noreferrer"
+      >
+        <LinkedInIcon sx={{ fontSize: 35, margin: 3 }} />
+      </a>
+      <a
+        target="_blank"
+        href="mailto:josh@glowingmanagement.com"
+        rel="noreferrer"
+      >
+        <EmailIcon sx={{ fontSize: 35, margin: 3 }} />
+      </a>
     </div>
   );
 };
